0bd1480ad80a4afad33cd29e4acbe724a09ddd48
joao-vtr-oliveira <joao.vtr.oliveira@outlook.com>
2024-01-29 17:40:01 -0300

    feat: change the background

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import WeatherCard from './components/WeatherCard'
 
 function App() {
   return (
-    <div className='h-screen flex flex-col justify-center items-center bg-black'>
+    <div className='h-screen flex flex-col justify-center items-center '>
       <WeatherCard />
     </div>
   )
