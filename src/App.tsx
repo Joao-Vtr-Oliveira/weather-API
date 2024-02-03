f30c0b5a48e9489e9ab7fca8ab322c1d4f5f57aa
joao-vtr-oliveira <joao.vtr.oliveira@outlook.com>
2024-02-03 11:24:38 -0300

    refactor: change the className to chakra props

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import { Box } from '@chakra-ui/react'
 import WeatherCard from './components/WeatherCard'
 
 function App() {
   return (
-    <div className='h-screen flex flex-col justify-center items-center '>
+    <Box h='100vh' display='flex' flexDirection='column' justifyContent='center' alignItems='center'>
       <WeatherCard />
-    </div>
+    </Box>
   )
 }
 
